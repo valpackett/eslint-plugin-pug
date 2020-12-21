047569e80d98561deba74f9959ce65e9c0b8c8e0
taichunmin <taichunmin@gmail.com>
2020-12-21 18:28:31 +0800

    Fix #7: indent error cause by auto fixing extra semi

--- test/index.test.js
+++ test/index.test.js
@@ -21,43 +21,68 @@ function execute (filename, baseConfig = {}, fix = false) {
   return fix ? results : _.get(results, 'messages')
 }
 
-test.each([
-  // https://eslint.org/docs/developer-guide/nodejs-api#cliengine
-  ['semi', 'semi', { rules: { semi: ['error', 'always'] } }],
-  ['simple', 'simple', { rules: { 'no-console': ['error', { allow: ['warn', 'error'] }] } }],
-  ['indent-setting', 'indent-setting', { rules: { indent: ['error', 2] } }],
-  ['standard', 'standard', {
-    extends: 'standard',
-    rules: {
-      'eol-last': ['error', 'never'],
-    }
-  }],
-  ['error-at-the-beginning', 'error-at-the-beginning', {
-    rules: {
-      'max-lines': ['error', { max: 1 }],
-      'max-len': ['error', { code: 35 }]
-    }
-  }],
-  ['code', 'code', { rules: { semi: ['error', 'always'] } }],
-])('lint %s toEqual snapshot %s.json with baseConfig %j', (filename, snapshot, config) => {
-  expect.hasAssertions()
-  const messages = execute(`${filename}.pug`, config)
-  const expected = require(`./fixtures/${snapshot}.json`)
-  expect(messages).toEqual(expected)
+function objSortKey (obj) {
+  if (_.isArray(obj)) return _.map(obj, objSortKey)
+  if (!_.isPlainObject(obj)) return obj
+  const grp = _.groupBy(_.toPairs(obj), pair => (_.isArray(pair[1]) || _.isPlainObject(pair[1])) ? 'b' : 'a')
+  _.each(grp.b, v => { v[1] = objSortKey(v[1]) })
+  return _.fromPairs([..._.sortBy(grp.a, '0'), ..._.sortBy(grp.b, '0')])
+}
+
+describe('snapshot unit test', () => {
+  let debug = { failed: true }
+
+  afterEach(() => {
+    if (debug.failed) console.log(JSON.stringify(objSortKey(debug)))
+    debug = { failed: true }
+  })
+
+  test.each([
+    // https://eslint.org/docs/developer-guide/nodejs-api#cliengine
+    ['semi', 'semi', { rules: { semi: ['error', 'always'] } }],
+    ['semi2', 'semi2', { rules: { semi: ['error', 'never'] } }],
+    ['simple', 'simple', { rules: { 'no-console': ['error', { allow: ['warn', 'error'] }] } }],
+    ['indent-setting', 'indent-setting', { rules: { indent: ['error', 2] } }],
+    ['standard', 'standard', {
+      extends: 'standard',
+      rules: {
+        'eol-last': ['error', 'never'],
+      }
+    }],
+    ['error-at-the-beginning', 'error-at-the-beginning', {
+      rules: {
+        'max-lines': ['error', { max: 1 }],
+        'max-len': ['error', { code: 35 }]
+      }
+    }],
+    ['code', 'code', { rules: { semi: ['error', 'always'] } }],
+  ])('lint %s toEqual snapshot %s.json with baseConfig %j', (filename, snapshot, config) => {
+    debug = { ...debug, filename, snapshot, config }
+    expect.hasAssertions()
+    const messages = execute(`${filename}.pug`, config)
+    debug.messages = messages
+    const expected = require(`./fixtures/${snapshot}.json`)
+    expect(messages).toEqual(expected)
+    debug.failed = false
+  })
+  
+  test.each([
+    ['semi', 'semi-fix', { rules: { semi: ['error', 'always'] } }],
+    ['semi2', 'semi2-fix', { rules: { semi: ['error', 'never'] } }],
+    ['string-interpolation', 'string-interpolation', { rules: { semi: ['error', 'always'] } }],
+    ['multiline', 'multiline', {
+      extends: 'standard',
+      rules: {
+        'eol-last': ['error', 'never'],
+      }
+    }],
+  ])('lint %s.pug toMatchObject snapshot %s.json with baseConfig %j', (filename, snapshot, config) => {
+    expect.hasAssertions()
+    const messages = execute(`${filename}.pug`, config, true)
+    debug.messages = messages
+    const expected = require(`./fixtures/${snapshot}.json`)
+    expect(messages).toMatchObject(expected)
+    debug.failed = false
+  })
 })
 
-test.each([
-  ['semi', 'semi-fix', { rules: { semi: ['error', 'always'] } }],
-  ['string-interpolation', 'string-interpolation', { rules: { semi: ['error', 'always'] } }],
-  ['multiline', 'multiline', {
-    extends: 'standard',
-    rules: {
-      'eol-last': ['error', 'never'],
-    }
-  }],
-])('lint %s.pug toMatchObject snapshot %s.json with baseConfig %j', (filename, snapshot, config) => {
-  expect.hasAssertions()
-  const messages = execute(`${filename}.pug`, config, true)
-  const expected = require(`./fixtures/${snapshot}.json`)
-  expect(messages).toMatchObject(expected)
-})
